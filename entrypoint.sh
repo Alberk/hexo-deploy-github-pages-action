fa2b7fecaeb1ac9130513a07de093b7406ed9f36
zhangshengjie <zhangshengjie@163.com>
2021-06-15 15:38:25 +0800

    initialize

---
+++ entrypoint.sh
@@ -0,0 +1,78 @@
+#!/bin/sh -l
+
+set -e
+
+# check values
+
+if [ -n "${PUBLISH_REPOSITORY}" ]; then
+    TARGET_REPOSITORY=${PUBLISH_REPOSITORY}
+else
+    TARGET_REPOSITORY=${GITHUB_REPOSITORY}
+fi
+
+if [ -n "${BRANCH}" ]; then
+    TARGET_BRANCH=${BRANCH}
+else
+    TARGET_BRANCH="gh-pages"
+fi
+
+if [ -n "${PUBLISH_DIR}" ]; then
+    TARGET_PUBLISH_DIR=${PUBLISH_DIR}
+else
+    TARGET_PUBLISH_DIR="./public"
+fi
+
+if [ -z "$PERSONAL_TOKEN" ]
+then
+  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+  exit 1
+fi
+
+REPOSITORY_PATH="https://x-access-token:${PERSONAL_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
+
+# deploy to
+echo "Deploy to ${TARGET_REPOSITORY}"
+
+# Installs Git.
+apt-get update && \
+apt-get install -y git && \
+
+# Directs the action to the the Github workspace.
+cd "${GITHUB_WORKSPACE}"
+
+echo "npm install ..."
+npm i -S hexo-prism-plugin -g
+npm install hexo-generator-search --save -g
+npm i hexo-permalink-pinyin  -g
+npm i --save hexo-wordcount -g
+npm install hexo-filter-github-emojis  -g
+npm install hexo-generator-feed  -g
+npm install
+
+echo "Clean folder ..."
+./node_modules/hexo/bin/hexo clean
+
+echo "Generate file ..."
+./node_modules/hexo/bin/hexo generate
+
+cd $TARGET_PUBLISH_DIR
+
+echo "Config git ..."
+
+# Configures Git.
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git remote add origin "${REPOSITORY_PATH}"
+
+git checkout --orphan $TARGET_BRANCH
+
+git add .
+
+echo 'Start Commit ...'
+git commit --allow-empty -m "Building and deploying Hexo project form Github Action"
+
+echo 'Start Push ...'
+git push -u origin "${TARGET_BRANCH}" --force
+
+echo "Deployment succesful!"
